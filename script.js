78c2fa377555ebbdae0d4e3de66f99311a1ad8e2
Peter Leite <peterleite.dev@gmail.com>
2021-10-09 13:59:50 -0300

    usando flatmap para separar os arrays

---
+++ script.js
@@ -0,0 +1,42 @@
+const front = "card_front";
+const back = "card_back";
+
+let techs = [
+    "bootstrap",
+    "css",
+    "electron",
+    "firebase",
+    "html",
+    "javascript",
+    "jquery",
+    "mongo",
+    "node",
+    "react"
+];
+
+createCards(techs);
+
+function createCards(techs) {
+    let cards = [];
+
+    for(let tech of techs){
+        cards.push(createCardPair(tech));
+    }
+    console.log(cards.flatMap(pair => pair));
+}
+
+function createCardPair(tech){
+    return [{
+        id: createId(tech),
+        icon:tech,
+        flipped: false,
+    }, {
+        id: createId(tech),
+        icon:tech,
+        flipped: false,
+    }]
+}
+
+function createId(tech){
+    return tech + parseInt(Math.random() * 1000)
+}
