a48582b4f0996bbdacf6c3cf38640f5025c5e797
Peter Leite <peterleite.dev@gmail.com>
2021-10-10 15:14:39 -0300

    add botão de mute

--- scripts/extra.js
+++ scripts/extra.js
@@ -1,2 +1,19 @@
 const missSound = document.getElementById('missSound');
 const hitSound = document.getElementById('hitSound');
+const muteBtn = document.querySelector('#muteBtn');
+const audios = document.querySelectorAll('audio');
+
+function muteSounds() {
+    audios.forEach(audio => {
+        if (audio.muted == false) {
+            audio.muted = true;
+        } else {
+            audio.muted = false;
+        }
+    })
+    changeBg(audios[0].muted)
+}
+
+function changeBg(boolean) {
+    boolean ? muteBtn.style.backgroundImage = 'url(./assets/feature/muted.svg)' : muteBtn.style.backgroundImage = 'url(./assets/feature/full.svg)'
+}
